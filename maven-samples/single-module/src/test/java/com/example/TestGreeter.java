5a562efed8416a475ae77062ef7b4e7054e6314c
Tashamax <139563112+Tashamax@users.noreply.github.com>
2023-10-21 18:09:28 +0100

    Removing extra test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,10 +35,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-  @Test
-  public void intentionally_Failed() {
-    assertTrue(true);
-  }
   
     @Test
   public void greetShouldIncludeGreetingMessage() {
